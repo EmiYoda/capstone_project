902952a4e27e630054156dad988688e1e2adf447
EmiYoda <emilianodelvalle@icloud.com>
2021-08-10 12:37:25 -0500

    changed cors origin for backend to deploy on vercel after failing in heroku

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://capstone-project-sooty.vercel.app/",
     })
 );
 app.use(express.json({ limit: "30mb" }));
